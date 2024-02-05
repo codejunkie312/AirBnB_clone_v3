65335bcb28d5325db4ba9495ab41bf6c595d06b4
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2024-02-05 21:33:52 +0100

    added 404 error page handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """placeholder"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,12 @@ app.register_blueprint(app_views, url_prefix="/api/v1")
 def close_session(session):
     """Closes the session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Returns a JSON-formatted 404 status code response"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
